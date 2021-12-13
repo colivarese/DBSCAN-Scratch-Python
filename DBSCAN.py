0a83b406bb320a63a5d97a066f59c073b2ddc676
colivarese <ces.oe97@gmail.com>
2021-12-13 12:54:42 -0600

    .

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.spatial.distance import cdist
+import matplotlib.pyplot as plt
+from random import randint
 
 class dbscan():
     def __init__(self, eps:float, minPts:int, n_iters:int) -> None:
@@ -8,17 +10,75 @@ class dbscan():
         self.n_iters = n_iters
 
     def fit(self, data):
+        data = np.asarray(data)
+        data = self.add_index(data)
+        C = dict()
         clusters = list()
+        noise = list()
         data = self.mark_as_unvisited(data)
-        random_start = np.random.randint(0, len(data),1)
-        data = self.mark_as_visited(data, random_start)
-        distances = self.get_minPts_distance_to_objects(data, random_start)
-        if (self.satisfy_minPts(distances)):
-            clusters.append(data[random_start,0:2])
-            for n in distances:
-                return
+        count = 0
+        #while (self.all_visited(data)):
         for _ in range(self.n_iters):
-            return
+            random_start = np.random.randint(0, len(data),1)[0]
+            isVisited = self.is_visited(data, random_start)
+            if not isVisited:
+                data = self.mark_as_visited(data, random_start)
+                distances = self.get_minPts_distance_to_objects(data, random_start)
+                C[count] = clusters
+                if (self.satisfy_minPts(distances)):
+                    #clusters.append(data[random_start,1:3])
+                    clusters.append(random_start)
+                    for p in distances:
+                        
+                        p = np.asarray(p)
+                        if not self.is_visited(data, p):
+                            data = self.mark_as_visited(data, p)
+                            p_dist = self.get_minPts_distance_to_objects(data,p)
+                            if (self.satisfy_minPts(p_dist)):
+                                #distances.append(p_dist)
+                                distances = np.concatenate((distances,p_dist))
+                                distances = np.asarray(list(set(distances)))
+                                a=1
+                        if not self.check_in_clusters(p, C):
+                        #if int(p) not in clusters:
+                            clusters.append(int(p))
+                
+                else:
+                    noise.append(random_start)
+                    data = self.mark_as_visited(data, random_start)
+            count += 1
+            clusters = []
+        C = {k:v for k,v in C.items() if v}
+        clusters = np.unique(clusters)
+        noise = np.unique(noise)
+        self.plot_clusters(data, C, noise)
+
+        
+    def check_in_clusters(self ,p, clusters):
+        for k in clusters:
+            c = clusters[k]
+            if p in c:
+                return True
+        return False
+
+    def plot_clusters(self, data, clusters, noise):
+        plt.figure()
+        colors = []
+        n = len(clusters)
+        for i in range(n):
+            colors.append('#%06X' % randint(0, 0xFFFFFF))
+        #colors = ['r','g','b']
+        for i, k in enumerate(clusters):
+            for p, c in zip(data, clusters[k]):
+                x = data[c,1]
+                y = data[c,2]
+                plt.scatter(x,y, color=colors[i])
+            for p, n in zip(data, noise):
+                x = data[n,1]
+                y = data[n,2]
+                plt.scatter(x,y, color='black')
+        plt.show()
+
 
     def mark_as_unvisited(self,data):
         num_cols = data.shape[0]
@@ -31,13 +91,26 @@ class dbscan():
         return data
 
     def get_minPts_distance_to_objects(self, data, idx):
-        distances = (cdist(data[:,0:2], data[idx,0:2] ,'euclidean'))
+        p = data[idx,1:3][...,None].T
+        distances = (cdist(data[:,1:3], p ,'euclidean'))
         eps_neigh = list()
-        for d in distances:
+        for idx, d in enumerate(distances):
             if d <= self.eps:
-                eps_neigh.append(d)
-            else:
-                return eps_neigh
+                eps_neigh.append(idx)
+        return eps_neigh
 
     def satisfy_minPts(self, distances):
         return len(distances) >= self.minPts
+
+    def add_index(self, data):
+        idx_col = np.array(range(0,len(data)))[...,None]
+        data_with_idx = np.append(idx_col, data, 1)
+        return data_with_idx
+
+    def is_visited(self, data, idx):
+        p = data[idx,:]
+        return p[-1] == 1
+
+    def all_visited(self, data):
+        tmp = np.min(data[:,-1])
+        return tmp == 0
--- main.py
+++ main.py
@@ -9,8 +9,15 @@ np.random.seed(42)
 def PointsInCircum(r,n=100):
     return [(math.cos(2*math.pi/n*x)*r+np.random.normal(-30,30),math.sin(2*math.pi/n*x)*r+np.random.normal(-30,30)) for x in range(1,n+1)]
 
-data = np.asarray(PointsInCircum(400,700))
+data_1 = np.asarray(PointsInCircum(200,700))
+#data_2 = np.asarray(PointsInCircum(800,700))
+#data = np.concatenate((data_1, data_2))
 
-DBSCAN = dbscan(20, 4, 100)
 
-DBSCAN.fit(data)
+DBSCAN = dbscan(200, 100, 200)
+
+DBSCAN.fit(data_1)
+
+#plt.figure()
+#plt.scatter(data[:,0], data[:,1])
+#plt.show()
